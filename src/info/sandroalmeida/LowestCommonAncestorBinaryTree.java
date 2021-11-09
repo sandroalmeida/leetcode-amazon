e5a26b0a28d2f665c6a51b6ba4882e26341e1130
sandroalmeida <sandrodotbuild@gmail.com>
2021-11-09 15:26:13 -0800

    LowestCommonAncestorBinaryTree

--- src/info/sandroalmeida/LowestCommonAncestorBinaryTree.java
+++ src/info/sandroalmeida/LowestCommonAncestorBinaryTree.java
@@ -18,4 +18,24 @@ public class LowestCommonAncestorBinaryTree {
             commonAncestor = currentNode;
         return (mid + left + right) > 0;
     }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.left.left.left = new TreeNode(8);
+        root.left.left.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(10);
+        root.left.right.right = new TreeNode(11);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        root.right.left.left = new TreeNode(12);
+        root.right.left.right = new TreeNode(13);
+        root.right.right.left = new TreeNode(14);
+        root.right.right.right = new TreeNode(15);
+
+        System.out.println(lowestCommonAncestor(root, root.left.left.right, root.left.right.right).val);
+    }
 }
