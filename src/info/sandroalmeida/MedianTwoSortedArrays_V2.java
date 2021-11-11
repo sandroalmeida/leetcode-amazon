b37bf00eac7c9858ca90d3aea1f2904e8f6728eb
sandroalmeida <sandrodotbuild@gmail.com>
2021-11-11 14:06:41 -0800

    MedianTwoSortedArrays_V2

---
+++ src/info/sandroalmeida/MedianTwoSortedArrays_V2.java
@@ -0,0 +1,14 @@
+package info.sandroalmeida;
+
+public class MedianTwoSortedArrays_V2 {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,3};
+        int[] nums2 = {2,4,5,9};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+}
